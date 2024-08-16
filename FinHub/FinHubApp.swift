fb43ecbaa85f42c487bac5b97bf7d0da8638837f
Priyabrata Chowley <prchowley@gmail.com>
2024-08-16 12:41:38 +0530

    Initial Commit

---
+++ FinHub/FinHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinHubApp.swift
+//  FinHub
+//
+//  Created by Priyabrata Chowley on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FinHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
